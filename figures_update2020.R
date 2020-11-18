c4c0d71a29f587640a7ef03ec7ee9836da444819
ShotaNishijima <nishijimash@gmail.com>
2020-11-18 16:19:46 +0900

    update figure code

--- figures_update2020.R
+++ figures_update2020.R
@@ -9,8 +9,8 @@ require(tidyverse)
 require(ggvast)
 
 # 0.1 set the directory ---------------------------------------------
-vast_output_dirname = "C:/Users/00007802/Dropbox/saVAST_egg/vast2020-08-27_lnorm_log100spottedfixed"
-fig_output_dirname = "C:/Users/00007802/Dropbox/saVAST_egg/vast2020-08-27_lnorm_log100spottedfixed"
+vast_output_dirname = "C:/Users/00007802/Dropbox/saba_egg/2020/vast2020-08-27_lnorm_log100_spotted_fixed"
+fig_output_dirname = "C:/Users/00007802/Dropbox/saba_egg/2020/vast2020-08-27_lnorm_log100_spotted_fixed"
 setwd(dir = vast_output_dirname)
 
 # 0.2 load the data -------------------------------------------------
@@ -39,8 +39,6 @@ plot_index(vast_index = vast_index,
            fig_output_dirname = fig_output_dirname)
 
 
-
-
 # 2. get dens --------------------------------------------------
 # make a data-frame
 df_dens = get_dens(category_name = category_name)
@@ -57,12 +55,12 @@ require(maps)
 #unique(map_data("world")$region)
 region = "Japan" #作図する地域を選ぶ
 scale_name = "Log density" #凡例　色の違いが何を表しているのかを書く
-ncol = 5 #横にいくつ図を並べるか（最大数 = 年数）
+ncol = 6 #横にいくつ図を並べるか（最大数 = 年数）
 shape = 16 #16はclosed dot
 size = 1 #shapeの大きさ
 
 # make figures
-g2 = map_dens(data = data,
+map_dens(data = data,
          region = region,
          scale_name = scale_name,
          ncol = ncol,
@@ -72,6 +70,7 @@ g2 = map_dens(data = data,
          zoom_in_lat = 1,zoom_in_lon=1)
 
 
+## ここまで　---------------------------------------------------
 
 # 4. get cog ----------------------------------------------------
 # make a data-frame
